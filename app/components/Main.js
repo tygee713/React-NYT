9070563374d4c0720a4fbd9d2ff2b332bd153bf8
Tyler Geerdts <tygee713@gmail.com>
2016-12-15 20:04:35 -0500

    Added children components

---
+++ app/components/Main.js
@@ -0,0 +1,17 @@
+var React = require("react");
+
+var Search = require("./children/Search.js");
+var Results = require("./children/Results.js");
+var Saved = require("./children/Saved.js");
+
+var helpers = require("./utils/helpers.js");
+
+var Main = React.CreateClass({
+  render = function() {
+    return (
+      
+    );
+  }
+});
+
+module.exports = Main;
---
+++ app/components/children/Saved.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var Saved = React.createClass({
+  render = function() {
+    return (
+      
+    );
+  }
+});
+
+module.exports = Saved;
