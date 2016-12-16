2f877edca1d96a48f701af14fc153741d8652fc1
Tyler Geerdts <tygee713@gmail.com>
2016-12-15 20:24:13 -0500

    Added HTML and functions for Main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -7,9 +7,53 @@ var Saved = require("./children/Saved.js");
 var helpers = require("./utils/helpers.js");
 
 var Main = React.CreateClass({
+  getInitialState: function() {
+    return { searchTerm: "", results: [], saved: [] };
+  },
+  componentDidMount: function() {
+
+  },
+  componentDidUpdate: function() {
+
+  },
+  setTerm: function(term) {
+    this.setState({ searchTerm: term });
+  },
   render = function() {
     return (
-      
+      <div className="container">
+        <div className="row">
+
+          <div className="jumbotron">
+
+            <h2 className="text-center">NYTimes Search</h2>
+            <p className="text-center">
+              <em>Enter a topic and start and end years and save your favorite results!</em>
+            </p>
+            
+          </div>
+
+          <div className="row">
+
+            <Search setTerm={this.setTerm} />
+
+          </div>
+
+          <div className="row">
+
+            <Results articles={this.state.results} />
+
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <Saved saved={this.state.saved} />
+
+        </div>
+
+      </div>
     );
   }
 });
